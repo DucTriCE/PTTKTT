a5e23158963276572681ed2ed8fce2351ac7208e
DucTriCE <trild.ce@gmail.com>
2023-10-05 00:52:52 +0700

    Finised bai13 theory and output theory file

--- hw01/bai13_theory.cpp
+++ hw01/bai13_theory.cpp
@@ -2,13 +2,19 @@
 #include <math.h>
 using namespace std;
 
+int sigma(int n){
+    int sum=0;
+    for(int i=0; i<=log2(n); i++)sum+=(n/(pow(2,i)));
+    return sum;
+}
+
 int main(){
     freopen("../input.txt","r",stdin);
     freopen("../output.txt","w",stdout);
     int n=1;
     while(n<=20){
-        int gan=(n*n)/2 + n + 2;
-        int sosanh= (n*n)/4 + n + 1;
+        int gan=2+2*(log2(n)+1)+2*sigma(n);
+        int sosanh=2*log2(n)+3+sigma(n);
         cout << "Case n=" << n << ": G(n)=" << gan << " SS(n)=" << sosanh << "\n";
         n++;
     }
