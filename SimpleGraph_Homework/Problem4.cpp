789a1e86b2e8bc81338f1414c339f468d952a840
DucTriCE <trild.ce@gmail.com>
2023-10-22 20:57:07 +0700

    Add finished problem 4

--- SimpleGraph_Homework/Problem4.cpp
+++ SimpleGraph_Homework/Problem4.cpp
@@ -4,26 +4,28 @@
 #include <map>
 using namespace std;
 
-int main()
-{
+int main(){
     freopen("../input.txt","r",stdin);
     freopen("../output.txt","w",stdout);
     int n;
     cin >> n;
-    vector<vector<int>> v(n, vector<int>(n));
     map<int, set<int>> mp;
+
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            cin >> v[i][j]
-            if(v[i][j]==1)mp[i+1].insert(j+1);
+            int x;
+            cin >> x;
+            if(x==1)mp[i+1].insert(j+1);
         }
     }
-    // for(map<int, set<int>>::iterator it; it!=mp.end(); it++){
-    //     cout << it->first << "->" << it->second.b
-    // }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++)cout<<v[i][j]<<" ";
-        cout << "\n";
+
+    for(map<int, set<int>>::iterator it=mp.begin(); it!=mp.end(); it++){
+        cout << it->first << "->";
+        for(set<int>::iterator st = it->second.begin(); st!=it->second.end(); st++){
+            cout << *st;
+            if(next(st)!=it->second.end())cout<<" ";
+            else cout << "\n";
+        }
     }
 }
 
