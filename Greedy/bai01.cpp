a764db97e9aa83e67e4e706e95df24460a3c0f89
DucTriCE <trild.ce@gmail.com>
2023-12-02 20:36:13 +0700

    Add solution for hw01 Greedy

--- Greedy/bai01.cpp
+++ Greedy/bai01.cpp
@@ -18,36 +18,32 @@ int get_minidx(vector<int> machine_time){
 }
 
 int main() {    
-    freopen("../input.txt","r",stdin);
-    freopen("../output.txt","w",stdout);
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
     int n, m;
     cin >> n >> m;
     vector<pair<int, int>> x(n);
-    vector<pair<int, int>> ans(n);
-
     vector<int> machine_time(m);
-    vector<int> out(n);
+    vector<int> sol(n);
 
     for(int i=0; i<n; i++){
         int tim;
         cin >> tim;
         x[i].first = tim;
-        x[i].second = -1;
-        ans[i].first = tim;
-        ans[i].second = -1;
+        x[i].second = i;
     }
+
     sort(x.rbegin(), x.rend()); 
     int min_idx = 0;     
 
     for(int i=0; i<n; i++){
         machine_time[min_idx]+=x[i].first;
-        ans[i].second = 
+        sol[x[i].second] = min_idx;
         int tmp = get_minidx(machine_time);
         if(tmp!=min_idx)min_idx = tmp;
     }
 
     for(int i=0; i<n; i++){
-        cout << x[i].second << " ";
+        cout << sol[i] << " ";
     } 
-
 }
