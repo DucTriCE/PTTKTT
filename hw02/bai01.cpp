63c90dfbc294e173951926bd4df221977d3a40d6
DucTriCE <trild.ce@gmail.com>
2023-12-10 10:57:26 +0700

    Add bai01 in hw02

--- hw02/bai01.cpp
+++ hw02/bai01.cpp
@@ -3,30 +3,39 @@
 
 using namespace std;
 
-void backtracking(int k){
-    for(int i=0; i<m; i++){
-        tmp_sol[k] = i;
-        machine_time[i]+=x[k];
-        if(k==n-1){
-            int tmp = findMaxtime();
-            if (tmp < mi){
-                sol = tmp_sol;
-                mi = tmp;
-            }
+vector<char> sol;
+char bracket[2] = {'(',')'};
+
+void output(vector<char> sol, int n){
+    for(int i=0; i<n*2; i++){
+        cout << sol[i];
+    }
+    cout << endl;
+}
+void backtracking(int k, int n, int isValid){
+    for(int i=0; i<2; i++){
+        sol[k] = bracket[i];
+        if(bracket[i]==')')isValid-=1;
+        else isValid+=1;
+        if(isValid<0)continue;
+        if(k==n*2-1){
+            // cout << isValid << " ";
+            if(isValid!=1)continue;
+            else output(sol, n);
         }
-        else backtracking(k+1);
-        machine_time[i]-=x[k];
+        else backtracking(k+1, n, isValid);
+
+        if(bracket[i]==')')isValid+=1;
+        else isValid-=1;
     }
 }
 
 int main() {    
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    freopen("../input.txt","r",stdin);
-    freopen("../output.txt","w",stdout);
+    // freopen("../input.txt","r",stdin);
+    // freopen("../output.txt","w",stdout);
     int n;
     cin >> n;
-    backtracking(0);
-    for(int i=0; i<n; i++){
-        cout << sol[i] << " ";
-    }
+    sol.resize(n*2);
+    backtracking(0, n, 0);
 }
