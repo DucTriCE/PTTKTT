772a0d107e368a41064f270bf62cb667a16aa62b
DucTriCE <trild.ce@gmail.com>
2023-12-17 16:30:47 +0700

    Add bonus05

---
+++ bonus5/bai01.py
@@ -0,0 +1,79 @@
+import random
+
+def insert(item, x):
+    mylst = []
+    for i in range(len(x) + 1):
+        tmp = x[:i] + [item] + x[i:]
+        # print("NEW PERM: ", new_perm)
+        mylst.append(tmp)
+    return mylst
+
+def permutation(A):
+    if not A:
+        return []
+    S = [[A[0]]]
+
+    for i in range(1, len(A)):
+        P = []
+        for x in S:
+            P.extend(insert(A[i], x))
+        S = P
+    return S
+
+'''-------------MAIN FUNCTION-----------------'''
+
+flag = False
+while True:
+    if not flag:
+        user_choice = int(input("Choose an option:\n1. Input an Array\n2. Create Random Array\nEnter choice (1 or 2): "))
+    if user_choice in [1, 2]:
+        if user_choice==1:
+            #Input from user
+            if not flag:
+                n = int(input("Number of elements in an array: "))
+            A = list(map(int, input("Enter an array separated by space: ").split()))
+
+            if len(A)>n:
+                print(f"\nPlease enter no more than {n} element !!!\n")
+                flag=True
+            elif len(A)<n:
+                print(f"\nPlease enter no less than {n} element !!!\n")
+                flag=True
+            else:
+                flag=False
+                results = permutation(A)
+                print(f"There are a total of {len(results)} permutations, including: ")
+                for item in results:
+                    print(item)
+
+                cont = int(input("\nDo you want to continue ?:\n1. Yes\n2. No\nEnter choice (1 or 2): "))
+                if cont == 1:
+                    print("\n")
+                    continue
+                elif cont==2:
+                    print("\nENDING PROGRAM !!!")
+                    break;
+                
+        else:
+            #Random array
+            A=[]
+            n = int(input("Number of elements in an array: "))
+            A = random.sample(range(0, 9), n)
+            print(f"Here is the random array that is created from {n} elements: {A}")
+
+            results = permutation(A)
+            print(f"There are a total of {len(results)} permutations, including: ")
+            for item in results:
+                print(item)
+    
+            cont = int(input("\nDo you want to continue ?:\n1. Yes\n2. No\nEnter choice (1 or 2): "))
+            if cont == 1:
+                print("\n")
+                continue
+            else:
+                print("ENDING PROGRAM !!!\n")
+                break
+    else:
+        print("\nWrong input format, please type again !!!\n")
+    
+'''----------------------------------------------'''
---
+++ bonus5/bai02.py
@@ -0,0 +1,74 @@
+import random
+
+def insert(item, x):
+    return x + [item]
+
+def gen_subset(A):
+    if not A:
+        return []
+    S = [[]]
+
+    for i in range(0, len(A)):
+        P = []
+        for x in S:
+            P.append(insert(A[i], x))
+        S.extend(P)
+    return S
+
+'''-------------MAIN FUNCTION-----------------'''
+
+flag = False
+while True:
+    if not flag:
+        user_choice = int(input("Choose an option:\n1. Input an Array\n2. Create Random Array\nEnter choice (1 or 2): "))
+    if user_choice in [1, 2]:
+        if user_choice==1:
+            #Input from user
+            if not flag:
+                n = int(input("Number of elements in an array: "))
+            A = list(map(int, input("Enter an array separated by space: ").split()))
+
+            if len(A)>n:
+                print(f"\nPlease enter no more than {n} element !!!\n")
+                flag=True
+            elif len(A)<n:
+                print(f"\nPlease enter no less than {n} element !!!\n")
+                flag=True
+            else:
+                flag=False
+                results = gen_subset(A)
+                print(f"There are a total of {len(results)} subsets, including: ")
+                for item in results:
+                    print(item)
+
+                cont = int(input("\nDo you want to continue ?:\n1. Yes\n2. No\nEnter choice (1 or 2): "))
+                if cont == 1:
+                    print("\n")
+                    continue
+                elif cont==2:
+                    print("\nENDING PROGRAM !!!")
+                    break;
+                
+        else:
+            #Random array
+            A=[]
+            n = int(input("Number of elements in an array: "))
+            A = random.sample(range(0, 9), n)
+            print(f"Here is the random array that is created from {n} elements: {A}")
+
+            results = gen_subset(A)
+            print(f"There are a total of {len(results)} subsets, including: ")
+            for item in results:
+                print(item)
+    
+            cont = int(input("\nDo you want to continue ?:\n1. Yes\n2. No\nEnter choice (1 or 2): "))
+            if cont == 1:
+                print("\n")
+                continue
+            else:
+                print("ENDING PROGRAM !!!\n")
+                break
+    else:
+        print("\nWrong input format, please type again !!!\n")
+    
+'''----------------------------------------------'''
