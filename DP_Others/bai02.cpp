9e3096ab8274af4bcc6679c532b0f58b07cb2f8b
DucTriCE <trild.ce@gmail.com>
2024-01-15 18:12:53 +0700

    Add homework DP bai02, 03

--- DP_Others/bai02.cpp
+++ DP_Others/bai02.cpp
@@ -1,29 +1,66 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include <set>
 using namespace std;
+
+//Reference: https://www.geeksforgeeks.org/graph-coloring-set-2-greedy-algorithm/
 
 int main(){
     freopen("../input.txt","r",stdin);
     freopen("../output.txt","w",stdout);
     int v, e;
     cin >> v >> e;
-    vector<string> vertex;
-    map<string, set<string>> Graph;
-    map<string, int> colors;
+
+    vector<int> colors(v, -1);
+    vector<vector<int>> G(v);     
+    map<string, int> vertexIndex; 
+
     for(int i=0; i<v; i++){
         string x;
         cin >> x;
-        vertex.push_back(x);
+        vertexIndex[x]=i;
     }
+
     for(int j=0; j<e; j++){
         string u, i;
         cin >> u >> i;
-        Graph[u].insert(i);
+        G[vertexIndex[u]].push_back(vertexIndex[i]);
+        G[vertexIndex[i]].push_back(vertexIndex[u]);
     }
-    for(int i=0; i<v; i++){
-        for(set<string>::iterator it=Graph[vertex[i]].begin(); it!=Graph[vertex[i]].end(); it++)
-            
+
+    colors[0] = 0;          //Mau dau tien luon bat dau la 0
+    vector<bool> usedColors(v, false);     //Tao ra mot mang gom v mau (do chi su dung toi da v mau) cua mot dinh dang xet, neu mau chua duoc su dung thi = false
+
+    for (int i = 1; i < v; i++) {
+
+        //Duyet cac dinh ke cua dinh dang xet
+        for (int adjacentVertex : G[i]) {
+            //Neu dinh ke do da duoc to mau
+            if (colors[adjacentVertex] != -1) {
+                //Ta set vi tri mau available tai dinh do thanh true (duoc su dung)
+                usedColors[colors[adjacentVertex]] = true;
+            }
+        }
+
+        // Tim ra mau dau tien hop le
+        int color;
+        for (color = 0; color < v; color++) {
+            //Neu mau tai do = false, tuc la mau dau tien chua duoc su dung tai bat ki dinh nao
+            if (usedColors[color]==false) break;
+        }
+        //Khi tim ra duoc value mau color, ta gan no vao mang ket qua
+        colors[i] = color;
+
+        //Khi qua mot dinh moi thi ta can reset lai mang usedColors de chuan bi xet dinh moi
+        for (int adjacentVertex : G[i]) {
+            if (colors[adjacentVertex] != -1) {
+                usedColors[colors[adjacentVertex]] = false;
+            }
+        }
+    }
+
+    // Output the colors of the vertices
+    for (int i=0; i<colors.size(); i++) {
+        cout << colors[i] << " ";
     }
 }
--- DP_Others/bai03.cpp
+++ DP_Others/bai03.cpp
@@ -1,31 +1,80 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <algorithm>
 using namespace std;
 
-vector<vector<int>> Graph;
-map<string, int> vertex_index;
-vector<int> color;
-int v, e;
+vector<int> colors, optimalColoring; 
+int minColors;                       // So mau it nhat da su dung
 
+void BackTracking(int k, vector<vector<int>> G, int v) {
 
-void BackTracking(int k){
-    for(int i=0; i<v; i++){
-        if(k==v-1){
+    for (int color=0; color<v; color++) {
 
+        if (color > minColors) break;   //Truong hop so mau su dung lon hon so mau it nhat da su dung
+
+        colors[k] = color; 
+        bool validColor = true;            
+
+        // Check cac dinh ke cua dinh hien tai co trung voi mau dang xet hay khong
+        for (int adj : G[k]) {
+            if (colors[adj] == colors[k]) {
+                validColor = false;
+                break;
+            }
         }
-        else BackTracking(k+1);
+
+        if (validColor) {
+            if (k == v - 1) {
+                //Tim ra value mau lon nhat su dung
+                int maxColor = *max_element(colors.begin(), colors.end());
+                //Xet voi value min va chi xet neu nho hon
+                if (maxColor < minColors) {
+                    minColors = maxColor;
+                    optimalColoring = colors;
+                }
+            } 
+            else BackTracking(k+1, G, v); // Move to the next vertex
+        }
     }
 }
 
-int main() {    
-    freopen("../input.txt","r",stdin);
-    freopen("../output.txt","w",stdout);
+int main() {
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    int v, e;               
     cin >> v >> e;
-    for(int i=0; i<v; i++){
+
+    colors.resize(v, -1), optimalColoring.resize(v, -1);        //mang so luong mau toi da su dung se bang so dinh
+
+    vector<vector<int>> G(v);     
+    map<string, int> vertexIndex;    
+
+    minColors = v - 1;              //Value cua mau duoc to toi da se bang so dinh tru 1 (Tu 0 den v-1)
+
+    for (int i=0; i<v; i++) {
         string x;
         cin >> x;
-        vertex_index[x] = i;
+        vertexIndex[x] = i;
     }
-    BackTracking(0);
-}
+
+    for (int j=0; j<e; j++) {
+        string u, i;
+        cin >> u >> i;
+        G[vertexIndex[u]].push_back(vertexIndex[i]);
+        G[vertexIndex[i]].push_back(vertexIndex[u]);
+    }
+
+    // for(int i=0; i<G.size(); i++){
+    //     for(int j=0; j<G[i].size(); j++)cout<<G[i][j]<<" ";
+    //     cout << endl;
+    // }
+
+    int k=0;
+    BackTracking(k, G, v);
+
+    for (int i=0; i<optimalColoring.size(); i++) {
+        cout << optimalColoring[i] << " ";
+    }
+}	
